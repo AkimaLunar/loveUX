85e55cbe50914e1f255580c7adb34c50f44e2e1d
Ria Carmin <akimalunar@gmail.com>
2014-12-01 14:26:36 -0800

    Added mobile friendly navigation
    a.k.a sliding side nav
    + some other media queries

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,38 @@
+//Responsive Side navigation
+function sideNavigationToggle(){
+	$('#side-navigation-toggle').on('click', function(event){
+		event.preventDefault();
+		//creating variables
+		var sideNavigation = $('#side-navigation');
+		var sideNavigationWidth = sideNavigation.width();
+		var overlay = $('#overlay');
+		var toggleButton = $('#side-navigation-toggle');
 
+		//toggling
+		sideNavigation.toggleClass('open');
+
+		//animation
+		if (sideNavigation.hasClass('open')) {
+			sideNavigation.animate({left: "0px"});
+			overlay.fadeIn();
+			toggleButton.hide();
+		} else {
+			sideNavigation.animate({left: -sideNavigationWidth}, 400);
+			overlay.fadeOut();
+			toggleButton.delay(300).fadeIn(200);
+		}
+
+		overlay.on('click', function(){
+			sideNavigation.animate({left: -sideNavigationWidth}, 400);
+			sideNavigation.removeClass('open');
+			overlay.fadeOut();
+			toggleButton.delay(300).fadeIn(200);
+		});
+
+	});
+}
+
+//Initialize
+(function(){
+	sideNavigationToggle();
+}());
